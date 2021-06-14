77242a404bb23489354e08376b94ca0e6d8a31ae
Likho1969 <likhokapesi04@gmail.com>
2021-06-14 15:56:10 +0200

    changes

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ root = tk.Tk()
 root.title("SIGN UP")
 root.geometry("1000x820")
 root.config(background='yellow')
-loader = Image.open("nation.webp")
+loader = Image.open("phanda.png")
 render = ImageTk.PhotoImage(loader)
 img = Label(root, image=render)
-img.place(x=0, y=0)
+img.place(x=400, y=130)
 
 
 # function
@@ -31,29 +31,29 @@ def logins():
 head = Label(root, text="YOUR TICKET TO MILLIONNERS", font=("bold", 20), bg="green", fg="white")
 head.grid(row=0, column=4)
 
-name = Label(text="NAME", fg="black", font=("bold", 18))
-name.grid(row=2, column=0)
+name = Label(text="NAME", fg="black", bg="yellow", font=("bold", 18))
+name.place(x=300, y=323)
 
-nm_entry = Entry(width=24, fg="black", font=("bold", 18))
-nm_entry.grid(row=2, column=6)
+nm_entry = Entry(width=24, fg="white", bg="black", font=("bold", 18))
+nm_entry.place(x=500, y=320)
 
-sname = Label(text="SURNAME", fg="black", font=("bold", 18))
-sname.grid(row=4, column=0)
+sname = Label(text="SURNAME", fg="black", bg="yellow", font=("bold", 18))
+sname.place(x=300, y=410)
 
-snm_entry = Entry(width=24, fg="black", font=("bold", 18))
-snm_entry.grid(row=4, column=6)
+snm_entry = Entry(width=24, fg="white", bg="black", font=("bold", 18))
+snm_entry.place(x=500, y=410)
 
 age = IntVar
 
-age_l = Label(text="AGE", fg="black", font=("bold", 18))
-age_l.grid(row=6, column=0)
+age_l = Label(text="AGE", fg="black", bg="yellow", font=("bold", 18))
+age_l.place(x=300, y=490)
 
-age_entry = Entry(textvariable=age, width=12, fg="black", font=("bold", 18))
-age_entry.grid(row=6, column=6)
+age_entry = Entry(textvariable=age, width=12, fg="white", bg="black", font=("bold", 18))
+age_entry.place(x=500, y=490)
 
 # button
 
-btn = Button(root, text="LOGIN", bg="magenta", command=logins)
-btn.grid(row=8, column=4)
+btn = Button(root, text="LOGIN", bg="green", fg="white", command=logins)
+btn.place(x=600, y=650)
 
 root.mainloop()
