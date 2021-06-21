cc379541cd28b190d1d2eec72a9288a9a680c879
Likho1969 <likhokapesi04@gmail.com>
2021-06-21 16:37:23 +0200

    last changes

--- currency.py
+++ currency.py
@@ -1,51 +1,50 @@
 
-# importing the required modules
 from tkinter import *
 from tkinter import messagebox
 import requests
 from PIL import Image,ImageTk
-# creating a window
+
 
 root = Tk()
-root.title("The Winner's Convert")   # naming the recently created window
-root.geometry("1170x390")   #  setting the size of the window
-root.config(bg="peach puff")   # setting the background color for the window
+root.title("ITHUBA National Lottery Currency Converter")
+root.geometry("1024x819")
 
-img = Image.open("Biggest-Currency-Movers.jpg")
+img = Image.open("time-currency-s.jpg")
 render = ImageTk.PhotoImage(img)
 
 img = Label(root, image=render)
 img.image = render
-img.place(x=0,y=0)
+img.place(x=0, y=0)
 
-myscrt = StringVar()
-myscrt.set("Default Currency is USD$")
+abc = StringVar()
+abc.set("Default Currency is USD$")
 
-r= requests.get("https://v6.exchangerate-api.com/v6/5f5691e2d32d65dbfe9f9300/latest/USD")
-print(r.json())
+req = requests.get(" https://v6.exchangerate-api.com/v6/7dba7e6bf61ba339ff735e21/latest/USD")
+print(req.json())
 
-l1 = Label(root,text="Money")
-l1.grid(row=2, column=0)
+l1 = Label(root, text="Winnings", font="sans-serif 12 bold", bg="green", fg="yellow")
+l1.place(x=100, y=300)
 
-e1 = Entry(root)
-e1. grid(row=4, column=0)
+entry1 = Entry(root)
+entry1.place(x=200, y=300)
 
 
-btn_active = Button(root, text = "Convert",command=r)
+btn_active = Button(root, text="Convert", font="sans-serif 12 bold", bg="green", fg="yellow", command=req)
 btn_active.grid(row=3, column=2)
 
-l2 = Label(root,text="Current Currency")
+l2 = Label(root, text="Current Currency", font="sans-serif 12 bold", bg="green", fg="yellow")
 l2.grid(row=2, column=5)
 
-e2 = Entry(root,state="readonly",textvariable=myscrt)
-e2.grid(row=4, column=5,pady=10)
+entry2 = Entry(root, state="readonly", textvariable=abc)
+entry2.grid(row=4, column=5, pady=10)
 
 
 # defining function that will exit/ close the window/ program
 def close():
-    close_it = messagebox.askyesno("Notice","Are you sure you want to exit this procedure?")
-    if close_it == "Yes":
+    query = messagebox.askquestion("ITHUBA National Lottery", "Are you sure you want to exit this procedure?")
+    if query == "yes":
         root.destroy()
+
 
 # exit button
 exit_btn = Button(text="Exit Program", command=close)
@@ -55,10 +54,11 @@ exit_btn.grid(row=9, column=5)
 result_entry = Entry(root)
 result_entry.grid(row=9, column=2)
 
+
 # defining function that will delete the figure in the Entry box
 def clear():
-    e1.delete(0)
-    e2.delete(0)
+    entry1.delete(0)
+    entry2.delete(0)
     result_entry.delete(0)
 
 
@@ -68,3 +68,4 @@ clear_btn.grid(row=9, column=1)
 
 
 # starting the app
+root.mainloop()
