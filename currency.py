1f566b57afe82081989fa4c905a058093ffefe11
Likho1969 <likhokapesi04@gmail.com>
2021-06-23 11:33:51 +0200

    Wednesday Changes

--- currency.py
+++ currency.py
@@ -1,98 +1,136 @@
+# Likho Kapesi
+
 from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
 import requests
-# from tkinter import messagebox
-
-root = Tk()
-
-# StringVar
-results = StringVar()
-values1 = StringVar()
-values2 = StringVar()
-
-# code to add widgets and style window will go here
-
-information = requests.get('https://v6.exchangerate-api.com/v6/89dcd9e8cc7777ded2575ce1/latest/USD')
-information_json = information.json()
-
-conversion_rates = information_json['conversion_rates']
-
-currency = []
-for i in conversion_rates.keys():
-    currency.append(i)
-
-currency2 = []
-for i in conversion_rates.keys():
-    currency2.append(i)
-
-currency_cb = ttk.Combobox(root)
-currency_cb['values'] = currency
-currency_cb['state'] = 'readonly'
-currency_cb.set('Select Currency')
-currency_cb.place(x=10, y=280)
+from PIL import Image, ImageTk
+from playsound import playsound
 
 
-currency_cb2 = ttk.Combobox(root)
-currency_cb2['values'] = currency2
-currency_cb2['state'] = 'readonly'
-currency_cb2.set('Select Currency')
-currency_cb2.place(x=253, y=280)
+root = tk.Tk()
+root.title("ITHUBA National Lottery- iCurrency Converter")
+root.geometry("1024x819")
+render = Image.open("time-currency-s.jpg")
+loader = ImageTk.PhotoImage(render)
+img = tk.Label(root, image=loader)
+img.place(x=0, y=0)
 
 
-Label(root, text='Enter Amount:', bg='#00A868').place(x=65, y=330)
-ent1 = Entry(root, bg='white')
-ent1.place(x=200, y=330)
-ent1.focus()
-Label(root, text='Converted Amount:', bg='#00A868').place(x=65, y=380)
-Label(root, text='', textvariable=results, bg='#00A868').place(x=200, y=380)
+class Converter:
+    def __init__(self):
+
+        self.results = StringVar()
+        self.values1 = StringVar()
+        self.values2 = StringVar()
+
+        self.information = requests.get('https://v6.exchangerate-api.com/v6/7dba7e6bf61ba339ff735e21/latest/USD')
+        self.information_json = self.information.json()
+
+        self.conversion_rates = self.information_json['conversion_rates']
+
+        self.currency = []
+        for i in self.conversion_rates.keys():
+            self.currency.append(i)
+
+        self.currency2 = []
+        for i in self.conversion_rates.keys():
+            self.currency2.append(i)
+
+        self.description_lbl = Label(root, text="1.Rapid currency conversion with ITHUBA National Lottery converter.", bg='yellow', fg="black", font="sans-serif")
+        self.description_lbl.place(x=20, y=20)
+        self.description_lbl2 = Label(root, text="2.Fast and easy to utilize currency converter. All major currencies included.", bg='yellow', fg="black", font="sans-serif")
+        self.description_lbl2.place(x=20, y=40)
+
+        self.currency_option = ttk.Combobox(root)
+        self.currency_option['values'] = self.currency
+        self.currency_option['state'] = 'readonly'
+        self.currency_option.set('Select From-Currency')
+        self.currency_option.place(x=200, y=180)
+
+        self.currency_option2 = ttk.Combobox(root)
+        self.currency_option2['values'] = self.currency2
+        self.currency_option2['state'] = 'readonly'
+        self.currency_option2.set('Select To-Currency',)
+        self.currency_option2.place(x=500, y=180)
+
+        self.lbl1 = Label(root, text='Enter Amount:', bg='green', fg="yellow", font="sans-serif")
+        self.lbl1.place(x=260, y=290)
+        self.entry1 = Entry(root, bg='yellow', fg="black", font="sans-serif")
+        self.entry1.place(x=400, y=290)
+        self.entry1.focus()
+        self.lbl2 = Label(root, text='Converted Amount:', bg='green', fg="yellow", font="sans-serif")
+        self.lbl2.place(x=230, y=350)
+        self.lbl3 = Label(root, text='', textvariable=self.results, bg='yellow', fg="black", font="sans-serif")
+        self.lbl3.place(x=410, y=350)
 
 
-def convert(from_currency, to_currency, amount):
-    # first convert it into USD if it is not in USD.
-    # because our base currency is USD
-    if from_currency != 'USD':
-        amount = amount / conversion_rates[from_currency]
+class Currency(Converter):
+    def __init__(self):
+        super(Currency, self).__init__()
+        def convert(currency1, currency2, amount):
+            try:
+                playsound('click.mp3')
+            except:
+                 print("Unrecognized audio format")
+            if currency1 != 'USD':
+                amount = amount / self.conversion_rates[currency1]
 
-    amount = round(amount * conversion_rates[to_currency], 4)
-    return amount
+            amount = round(amount * self.conversion_rates[currency2], 4)
+            return amount
+
+        def calc():
+            try:
+                amount = float(self.entry1.get())
+                currency1 = self.currency_option.get()
+                currency2 = self.currency_option2.get()
+
+                amount_converted = convert(currency1, currency2, amount)
+
+                self.results.set(amount_converted)
+            except ValueError:
+                if self.entry1 != int and self.entry1 != float:
+                    try:
+                        playsound('beep-05.mp3')
+                    except:
+                        print("Unrecognized audio format")
+                    messagebox.showerror('INVALID', 'You Are Advised to Enter numbers only')
+
+            except requests.exceptions.ConnectionError:
+                messagebox.showerror('ERROR', 'Internet Connection is Bad')
+            except KeyError:
+                messagebox.showerror('ERROR', 'Please Select a Currency')
+
+        # kill program
+        def close():
+            try:
+                playsound('button-4.mp3')
+            except:
+                print("Unrecognized audio format")
+            query = messagebox.askquestion("Closing Application", "Do you want to exit the application?")
+            if query == "yes":
+                root.destroy()
+
+        def clear():
+            try:
+                playsound('click.mp3')
+            except:
+                 print("Unrecognized audio format")
+            self.currency_option.set('Select Currency')
+            self.currency_option2.set('Select Currency')
+            self.entry1.delete(0, END)
+            self.entry1.focus()
+            self.results.set('')
+
+        self.convert_btn = Button(root, text="CONVERT", borderwidth=3, bg='lime', fg="black", font="sans-serif", command=calc)
+        self.convert_btn.place(x=230, y=480)
+        self.clear_btn = Button(root, text="CLEAR", borderwidth=3, bg='lime', fg="black", font="sans-serif", command=clear)
+        self.clear_btn.place(x=380, y=480)
+        self.exit_btn = Button(root, text="EXIT", bg="red", fg="black", borderwidth=3, font="sans-serif", command=close)
+        self.exit_btn.place(x=500, y=480)
 
 
-def perform():
-    try:
-        amount = float(ent1.get())
-        from_curr = currency_cb.get()
-        to_curr = currency_cb2.get()
-
-        converted_amount = convert(from_curr, to_curr, amount)
-
-        results.set(converted_amount)
-    except ValueError:
-        if ent1 != int and ent1 != float:
-            messagebox.showerror('Entry not valid', 'Enter numbers only')
-
-    except requests.exceptions.ConnectionError:
-        messagebox.showerror('Internet error', 'Internet Bad')
-    except KeyError:
-        messagebox.showerror('ERROR!!!!!!!!!!!!!!!', 'Select Currency')
-
-
-# kill program
-def kill():
-    return root.destroy()
-
-
-def clear():
-    currency_cb.set('Select Currency')
-    currency_cb2.set('Select Currency')
-    ent1.delete(0, END)
-    ent1.focus()
-    results.set('')
-
-
-Button(root, text="CONVERT", borderwidth=3, bg='white', command=perform).place(x=180, y=430)
-Button(root, text="EXIT", borderwidth=3, bg='white', command=kill).place(x=281, y=480)
-Button(root, text="CLEAR", borderwidth=3, bg='white', command=clear).place(x=100, y=480)
-
-
-root.mainloop()  # continuously runs program in window
+app = Converter()
+app2 = Currency()
+root.mainloop()
